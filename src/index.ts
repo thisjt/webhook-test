ed0d031de7eed3c31bfffb4f48e1acdbead05dc8
kotaro266 <elisha.alfeche08@gmail.com>
2025-11-21 07:13:34 +0800

    refactor: post test, change compat date

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get('/test', (c) => c.text('Hono!'))
+app.post('/test', async (c) => {
+	const bodyText = await c.req.text();
+	const headers = c.req.header();
+	console.log('Query:', c.req.query());
+	console.log(`Headers:`, headers);
+	console.log(`Body [${bodyText.length}]:`, bodyText);
+	return c.json({ message: 'test' });
+});
 
 export default app;
